18aca15c9b5b631bf91d11f4a989605c51c1a618
Jackie <jackie.cai@kiwinano.com>
2019-12-11 21:17:38 -0500

    Test1

---
+++ UnitTestLearn/Test1.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ABBY
+ * Date: 2019-12-11
+ * Time: 12:22 PM
+ */
+
+namespace UnitTestLearn;
+
+use Test1;
+
+class TestClass extends \PHPUnit_Framework_TestCase
+{
+    public $value1;
+    public $value2;
+
+    protected function setUp()
+    {
+        $this->value1 = 2;
+        $this->value2 = 3;
+    }
+
+    public function testPass()
+    {
+        $this->assertTrue($this->value1 + $this->value2 == 13);
+    }
+
+}
